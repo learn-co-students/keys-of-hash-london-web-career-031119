58888f6b025beebf9c30ca9a133befe0f69d4119
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-06 14:37:48 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    key_matches = []
+    self.each do |key, value|
+      arguments.any? { |arg| arg == value} ? key_matches << key : nil
+    end
+    key_matches
   end
 end
