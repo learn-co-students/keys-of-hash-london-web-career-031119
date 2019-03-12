e1617d9616b8563950596426439e887a6c7b28c4
Fernando <fernando_lopezc@live.com>
2019-03-12 18:06:34 +0000

    fer

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+require 'pry'
+=begin
+This method should take an undefined number of arguments, 
+using the splat operator, an2d return an array with every key from the hash whose value matches the value(s) given as an argument.
+=end
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = []
+    p arguments
+    
+  self.each do |k,v|
+    # binding.pry
+    arguments.each do |val|
+      if v == val
+        arr << k  
+      end
+    end  
+  end
+  arr
   end
 end
